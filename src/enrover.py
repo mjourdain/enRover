53ffac39934ad1749f9462d5cfc055ba7baf26af
Mathieu Briand <briandmathieu@hyprua.org>
2011-06-07 13:40:05 +0200

    Load file passed on arguments

--- src/enrover.py
+++ src/enrover.py
@@ -3,28 +3,18 @@
 
 import sys
 import random
-from PyQt4 import QtGui
+from PyQt4 import QtGui, QtCore
 from carte import Carte
 from ms import MS
 from bts import BTS
 
 app = QtGui.QApplication(sys.argv)
+args = QtCore.QCoreApplication.arguments()
 
 carte = Carte(800, 600)
 
-#carte.load_file("conf.xml")
-
-#carte.add(BTS(20, 400, "GSM"))
-#carte.add(BTS(130, 210, "GSM"))
-#carte.add(BTS(80, 45, "UMTS"))
-#carte.add(BTS(400, 300, "UMTS"))
-
-#carte.add(MS(random.randint(0, 799), random.randint(0, 599), "GSM"))
-#carte.add(MS(random.randint(0, 799), random.randint(0, 599), "GSM"))
-#carte.add(MS(random.randint(0, 799), random.randint(0, 599), "GSM"))
-#carte.add(MS(random.randint(0, 799), random.randint(0, 599), "UMTS"))
-#carte.add(MS(random.randint(0, 799), random.randint(0, 599), "UMTS"))
-#carte.add(MS(random.randint(0, 799), random.randint(0, 599), "UMTS"))
+if (len(args) == 2):
+  carte.load_file(args[1])
 
 carte.start_moving_ms()
 
