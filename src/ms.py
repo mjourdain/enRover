eb562da580cff0d4d702d0200c2c665f0f79c69c
Mathieu Briand <briandmathieu@hyprua.org>
2011-06-07 13:14:49 +0200

    Handover call interval

--- src/ms.py
+++ src/ms.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from station import Station
+from PyQt4 import QtCore
 import random
 import operator
 
@@ -13,15 +14,27 @@ class MS(Station):
     self.bts = None
     self.pref_network = network
     self.p = p
+
+    self.__bts_mutex = QtCore.QMutex()
+
     self.__last_move = random.randint(0, 7)
+
+    self.__handover_timer = QtCore.QTimer()
+    self.__handover_timer.timeout.connect(self.handover)
+    self.__handover_timer.setInterval(480)
+    self.__handover_timer.start()
 
   def update_bts_list(self, bts_list):
     """Update known Base Transmiter Station list"""
+    self.__bts_mutex.lock()
+
     self.__bts_list = bts_list
 
     # Initialize by choosing closest BTS
     dists = [ (bts, self.distance_from(bts)) for bts in self.__bts_list ]
     self.bts = min(dists, key=operator.itemgetter(1))[0]
+
+    self.__bts_mutex.unlock()
 
   def random_move(self, max_x, max_y):
     """Move Mobile Station randomly"""
@@ -62,4 +75,13 @@ class MS(Station):
     elif self.pos_y > max_y:
       self.pos_y = max_y
       self.__last_move = (self.__last_move + 4) % 8
+
+  def handover(self):
+    """Check if we need do do an handover"""
+    self.__bts_mutex.lock()
+
+    if self.bts is None:
+      return
+
+    self.__bts_mutex.unlock()
 
